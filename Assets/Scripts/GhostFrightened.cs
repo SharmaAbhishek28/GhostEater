67ddbcbe82099de1ca40ddbb21f471787b7e7695
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-10 09:12:01 +0530

    Ghost Frightened add

--- Assets/Scripts/GhostFrightened.cs
+++ Assets/Scripts/GhostFrightened.cs
@@ -4,15 +4,112 @@ using UnityEngine;
 
 public class GhostFrightened : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer body;
+    public SpriteRenderer eyes;
+    public SpriteRenderer blue;
+    public SpriteRenderer white;
+
+     public bool eaten { get; private set; }
+
+      public override void Enable(float duration)
     {
-        
+        base.Enable(duration);
+
+        this.body.enabled = false;
+        this.eyes.enabled = false;
+        this.blue.enabled = true;
+        this.white.enabled = false;
+
+        Invoke(nameof(Flash), duration / 2.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+     public override void Disable()
     {
-        
+        base.Disable();
+
+        this.body.enabled = true;
+        this.eyes.enabled = true;
+        this.blue.enabled = false;
+        this.white.enabled = false;
+
+    }    
+    
+     private void Flash()
+    {
+        if (!this.eaten)
+        {
+            this.blue.enabled = false;
+            this.white.enabled = true;
+            this.white.GetComponent<AnimatedSprite>().Restart();
+        }
     }
+
+     private void Eaten()
+    {
+        this.eaten = true;
+        Vector3 position = this.ghost.home.inside.position;
+        position.z = this.ghost.transform.position.z;
+        this.ghost.transform.position = position;
+        this.ghost.home.Enable(this.duration);
+        
+        this.body.enabled = false;
+        this.eyes.enabled = true;
+        this.blue.enabled = false;
+        this.white.enabled = false;
+       
+    }
+
+      private void OnEnable()
+    {
+        blue.GetComponent<AnimatedSprite>().Restart();
+        this.ghost.movement.speedMultiplier = 0.5f;
+        this.eaten = false;
+    }
+
+    private void OnDisable()
+    {
+        this.ghost.movement.speedMultiplier = 1.0f;
+        this.eaten = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (this.enabled) {
+                Eaten();
+            }
+            
+        }
+    }
+
+     private void OnTriggerEnter2D(Collider2D other)
+    {
+        Node node = other.GetComponent<Node>();
+
+        // Do nothing while the ghost is frightened
+        if (node != null && this.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float maxDistance = float.MinValue;
+
+            // Find the available direction that moves closet to pacman
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                // If the distance in this direction is less than the current
+                // min distance then this direction becomes the new closest
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude;
+
+                 if (distance > maxDistance)
+                {
+                    direction = availableDirection;
+                    maxDistance = distance;
+                }
+            }
+
+            this.ghost.movement.SetDirection(direction);   
+        }
+
+    } 
 }
